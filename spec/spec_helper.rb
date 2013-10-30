082b9376fdbc49dc4ae903ebb68a564c3b5d4766
Michael Klett <moklett@gmail.com>
2013-10-30 17:06:25 -0400

    Updates VCR to ~> 2.0 and filters credentials from cassettes
    Specifically, I am now using 2.6.0 of VCR

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,4 +11,7 @@ end
 VCR.config do |c|
   c.cassette_library_dir = File.dirname(__FILE__) + '/support/vcr_cassettes'
   c.stub_with :fakeweb
+  c.filter_sensitive_data('<ns0:username>[GATEWAY_LOGIN]</ns0:username>') { "<ns0:username>#{GATEWAY_LOGIN}</ns0:username>" }
+  c.filter_sensitive_data('<ns0:password>[GATEWAY_PASSWORD]</ns0:password>') { "<ns0:password>#{GATEWAY_PASSWORD}</ns0:password>" }
+  c.filter_sensitive_data('<ns0:merchantNumber>[GATEWAY_MERCHANT_NUMBER]</ns0:merchantNumber>') { "<ns0:merchantNumber>#{GATEWAY_MERCHANT_NUMBER}</ns0:merchantNumber>" }
 end
